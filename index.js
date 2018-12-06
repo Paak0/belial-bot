2ba4fd4fa853af688db4c196ed33965f4aa617fb
Paak0 <41326576+Paak0@users.noreply.github.com>
2018-12-06 12:30:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ bot.on('message', message => {
     if (command[0] === '!') {
         if(command === commands[0]){
             let url = '';
-            let mention = message.mentions.users.first();
+            let mention = message.mentions.users.array().first();
             
             if(mention){
-                url = message.mentions.users[0].avatarURL;
+                url = message.mentions.users.array()[0].avatarURL;
                 message.channel.sendMessage("mention is up");
-                message.channel.sendMessage(message.mentions.users[0].username);
+                message.channel.sendMessage(message.mentions.users.array()[0].username);
             }else{
                 url = message.author.avatarURL;
             }
