8734b5723561d5e5f1e70e71ebe1eb678f4487a7
Paak0 <41326576+Paak0@users.noreply.github.com>
2018-12-06 01:19:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ bot.on('message', message => {
     let command = message.content;
     if (command[0] === '!') {
         if(command === commands[0]){
-            let avatar = new RichEmbed();
-            avatar.setTitle('Avatar link');
-            avatar.setURL(message.author.avatarURL);
-            message.channel.sendMessage(avatar);
+            let embed = new Discord.RichEmbed();
+            embed.setTitle('Avatar link');
+            embed.setURL(message.author.avatarURL);
+            message.channel.sendMessage(embed);
         }
         
         
