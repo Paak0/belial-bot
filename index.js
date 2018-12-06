b93350c5c60814839cb7dab0d95c68c2d42b4cd1
Paak0 <41326576+Paak0@users.noreply.github.com>
2018-12-06 13:44:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.on('ready', () => {
 bot.on('message', message => {
     
     if(!message.author.bot){
-        message.channel.send(message.users.first().avatarURL);
+        message.channel.send(message.members.first().user.avatarURL);
         let command = message.content;
 
         if(command === 'ping') message.channel.send('pongogongo');
