0706db229cc8275855f7d80ce1cf3c741674597d
Paak0 <41326576+Paak0@users.noreply.github.com>
2018-12-10 14:31:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ Use me for whatever you want.`
 						return;
 					}
 					currentlyPlayed = song.title;
-					dispatcher = message.guild.voiceConnection.playStream(yt(song.url, { filter: 'audioonly' , quality: 'lowest' }), { passes : 3 });
+					dispatcher = message.guild.voiceConnection.playStream(yt(song.url, { filter: 'audioonly' , quality: 'lowest' }), { passes : 4 });
 					dispatcher.on('end', () => {
 						play(disServ[message.guild.id].songs.shift());
 					});
