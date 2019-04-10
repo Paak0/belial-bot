449cb1403ed6e35e2d9a61608ebe135710b42154
Paak0 <41326576+Paak0@users.noreply.github.com>
2019-04-10 19:05:08 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,324 +1,194 @@
 
 const request = require('superagent');
 const Discord = require('discord.js');
+const fs = require('fs');
 const bot = new Discord.Client();
-
 const config = require('./src/config.js');
 const botToken = config.token || process.env.BOT_TOKEN;
 const commands = require('./src/commands.js');
 const characters = require('./src/characters.json');
-const commandPrefix = '.';
+bot.belial = require('./src/belial.js');
 
-const commandsNames = [
-	'help', 'random', 'info', 'avatar', 'slap', 
-	'join', 'leave', 'add', 'skip', '[ l, list, q, queue ]', '[ c, clear ]'
-];
-
-const belialWords = require('./src/belial.js').words;
-
-let servers = {};
-let user;
-let mention;
-let randomNumber;
-let voiceChannel;
-let dispatcher;
-let currentlyPlayed;
-let counter = 0;
-let lastMsg = '';
-
+//========================================================   gbf sounds
 let soundNames = ['them', 'us', 'ability_them', 'ability_us', 'mypage', 'cutin', 
 				  'win', 'lose', 'attack', 'kill', 'ready', 'mortal', 'damage', 
 				  'dying', 'zenith_up', 'runk_up', 'introduce', 'evolution', 'formation', 
 				  'archive', 'to_player', 'healed', 'helaled', 'hp_down', 'power_down', 'player_gauge', 'special'];
 let adds = ['', 'a', 'b', '_a', '_b', '_mix'];
 let sounds = [];
+//========================================================
 
+
+bot.commands = new Discord.Collection();
+
+fs.readdir('./src/commands/', (e, f) => {
+	if(e) return console.log(e);
+	f.map( c => {
+		let cmd = require(`./src/commands/${c}`);
+		bot.commands.set(cmd.name, cmd);
+	});
+	console.log(`Loading commands: done.`);
+});
 
 bot.on('ready', () => {
-	bot.guilds.map( guild => { 
-		if(!servers[guild.id]) servers[guild.id] = {
-			name: guild.name,
+	bot.guilds.map( g => {
+		g.cmdPrefix = '.';
+		g.msgCounter = 0;
+		g.lastMsg = '';
+		g.music = {
 			playing: false,
 			songs: [],
-			currentVoiceChannel: null
-		}
+			voiceChannel: null,
+			currentlyPlayed: '',
+			dispatcher: {}
+		};
 	});
-    console.log('Belial is ready to serve.');
+	
+    console.log(`
+#################################\n
+    Belial is ready to serve.\n
+#################################\n`);
+	console.log(`Connected to ${bot.guilds.size} servers:\n${bot.guilds.map( g => '- '+g.name ).join('\n')}\n`);
 });
 
-bot.on('disconnect', () => {
-    console.log('Belial going to rest. Need more libido.');
-});
-
-bot.on('reconnecting', () => {
-    console.log('Reconnecting.');
-});
 
 bot.on('message', async message => {
     if(message.author.bot) return;
-	
 	let words = message.content.toLowerCase().split(' ');
+	let msgGuild = bot.guilds.get(message.guild.id);
 	
 	if(words[0].startsWith('<:')){
-		if(message.content === lastMsg){
-			if(counter == 2){
+		if(message.content === msgGuild.lastMsg){
+			if(msgGuild.msgCounter == 2){
 				let emo = bot.emojis.find(emoji => emoji.name === words[0].split(':')[1]) || 0;
 				if(emo) message.channel.send(words[0]);
-				counter = 0;
-				lastMsg = '';
+				bot.guilds.get(message.guild.id).msgCounter = 0;
+				bot.guilds.get(message.guild.id).lastMsg = '';
 			}else{
-				counter++;
+				bot.guilds.get(message.guild.id).msgCounter++;
 			}
 		}else{
-			counter = 1;
-			lastMsg = message.content;
+			bot.guilds.get(message.guild.id).msgCounter = 1;
+			bot.guilds.get(message.guild.id).lastMsg = message.content;
 		}
-		console.log(counter);
 	}
 	
 	words.filter( function(word){
-		if(belialWords.indexOf(word) != -1){
-			message.react(bot.emojis.find(emoji => emoji.name === 'ppp'));
-			return;
-		}
+		if(bot.belial.words.indexOf(word) != -1) return message.react(bot.emojis.find(emoji => emoji.name === 'ppp'));
 	});
 	
-	if(words[0].startsWith(commandPrefix)){
-		if(words[0][1].includes('.')) return;
-		let cmd = words[0].substring(1);
+	if(words[0].startsWith(msgGuild.cmdPrefix)){
+		if(words[0][1].includes(msgGuild.cmdPrefix)) return;
 		
-		switch(cmd){
-			case 'h':
-			case 'help':
-				message.channel.send(`
-**Commands:**
-${commandsNames.map( (com, index) => `${com}`).join(', ')}
-
-Use me for whatever you want.`
-				);
-				break;
-				
-			case 'random': commands.random.dothis(message);
-				break;
-				
-			case 'avatar': commands.avatar.dothis(message);
-				break;
-				
-			case 'slap': commands.slap.dothis(message);
-				break;
-				
-			case 'gay':
-			case 'gey':
-			case 'gai':
-			case 'gei':
-			case 'ugay':
-			case 'ugai':
-			case 'ugei':
-			case 'ugey':
-				message.channel.send('No, u.');
-				break;
-				
-			case 'info': commands.info.dothis(message);
-				break;
-				
-			case 'join':
-				commands.join.dothis(message, servers[message.guild.id]);
-				// if (!disServ[message.guild.id] || !disServ[message.guild.id].songs[0]) disServ[message.guild.id] = {}, disServ[message.guild.id].playing = false, disServ[message.guild.id].songs = [];;
-				// voiceChannel = message.member.voiceChannel;
-				// if(voiceChannel) voiceChannel.join();
-				// else message.channel.send(message.guild.id);
-				break;
-				
-			case 'leave':
-				commands.leave.dothis(message, servers[message.guild.id]);
-				// voiceChannel = message.member.voiceChannel;
-				// if(voiceChannel) voiceChannel.leave();
-				// else message.channel.send('Notto in voice channeru.');
-				break;
-				
-			case 'add':
-				commands.add.dothis(message, servers[message.guild.id]);
-				// if(!message.member.voiceChannel) return message.react('🔇');
-				// if(!words[1]) return;
-				// let searchString = words.slice(1).join(' ');
-				// let msgAuthor = message.author.username;
-				
-				// let videos = await youtube.searchVideos(searchString, 10);
-				
-				// message.channel.send(`
-					// ${videos.map( (vid, index) => `**${++index}. ** ${vid.title}`).join('\n')}
-				// `).catch( err => {
-					// console.log(err);
-					// return message.channel.send('Not my bad. Youtube dumb.');
-				// });
-				
-				// let songIndex;
-				
-				// await message.channel.awaitMessages(msg => msg.content > 0, {
-					// max: 1,
-					// time: 15000,
-					// errors: ['time'],
-				// })
-				// .then((collected) => {
-					// if(!collected) return;
-					// songIndex = collected.first().content;
-				// })
-				// .catch(() => {
-					// message.channel.send('No correct song picked.');
-					// return;
-				// });
-				// if(!songIndex) break;
-				
-				// if(videos[songIndex - 1].durationSeconds > 420) return message.channel.send('Video too long.');
-				
-				// let url = videos[songIndex - 1].url;
-				
-				// yt.getInfo(url, (err, info) => {
-					// if(err) return message.channel.send('Shitty link.');
-					// servers[message.guild.id].songs.push( {url: url, title: info.title, requester: message.author.username} );
-					// message.channel.send(`\`\`\`Added: ${info.title}\`\`\``);
-				// });
-				break;
-				
-			case 'l':
-			case 'list':
-			case 'q':
-			case 'queue':
-				if(!message.member.voiceChannel){
-					message.react('🔇');
-					return;
-				}
-				if (!servers[message.guild.id].songs[0]) return message.channel.send(`Nothing.`);
-				let tosend = [];
-				servers[message.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.title} - Requested by: ${song.requester}`);});
-				message.channel.send(`__**Music Queue:**__ Currently **${tosend.length}** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
-				break;
-				
-			case 'c':
-			case 'clear':
-				if(!message.member.voiceChannel){
-					message.react('🔇');
-					return;
-				}
-				let index = words[1] || null;
-				if(index > 0 && index < servers[message.guild.id].songs.length + 1){
-					let title = servers[message.guild.id].songs.splice(index - 1, 1).title;
-					message.channel.send(`Song: ${title} deleted.`);
-				}else{
-					servers[message.guild.id].songs = [];
-					message.channel.send(`List cleared.`);
-				}
-				break;
-				
-			case 'play':
-				if(!message.member.voiceChannel){
-					message.react('🔇');
-					return;
-				}
-				if (!servers[message.guild.id].songs[0]) return message.channel.send('Nothing to play.');
-				if (servers[message.guild.id].playing) return message.channel.send("I'm already playing!!!");
-				
-				servers[message.guild.id].playing = true;
-				
-				(function play(song) {
-					dispatcher = {};
-					if (!song){
-						servers[message.guild.id].playing = false;
-						return;
-					}
-					currentlyPlayed = song.title;
-					
-					dispatcher = message.guild.voiceConnection.playStream(yt(song.url, { filter: 'audioonly' }), { seek: 1, passes: 4 });
-					dispatcher.on('end', () => {
-						play(servers[message.guild.id].songs.shift());
-					});
-					dispatcher.on('error', () => {
-						return message.channel.sendMessage('Shit happened.').then(() => {
-							play(servers[message.guild.id].songs.shift());
-						});
-					});
-				})(servers[message.guild.id].songs.shift());
-				break;
-				
-			case 'np': if(servers[message.guild.id].playing) message.channel.send(`\`\`\`Playing: ${currentlyPlayed}\`\`\``);
-				break;
-				
-			case 'skip':
-				if(!message.member.voiceChannel){
-					message.react('🔇');
-					return;
-				}
-				dispatcher.end();
-				break;
-				
-			case 'yomomma': commands.yomomma.dothis(message);
-				break;
-				
-			case 'wisdom': commands.wisdom.dothis(message);
-				break;
-				
-			case 'trump': commands.trump.dothis(message);
-				break;
-				
-			case 'test':
-				sounds = [];
-				let count = 1;
-				
-				for(let i = 0; i < soundNames.length; i++){
-					for(let j = 1; j < 4; j++){
-						for(let k = 0; k < adds.length; k++){
-							request.head('http://game-a5.granbluefantasy.jp/assets/sound/voice/'+ words[1] +'_'+soundNames[i]+j+adds[k]+'.mp3').then( res => {
-								if(!sounds.includes(soundNames[i])){
-									sounds[i] = soundNames[i];
-								}else{}
-								count++;
-							}).catch(e => {
-								count++;
-							});
-						}
-					}
-				}
-				
-				for(let i = 0; i < soundNames.length; i++){
-					for(let j = 1; j < 4; j++){
-						request.head('http://game-a5.granbluefantasy.jp/assets/sound/voice/'+ words[1] +'_'+soundNames[i]+'0'+j+'.mp3').then( res => {
-							if(!sounds.includes(soundNames[i])){
-									sounds[i] = soundNames[i];
-							}else{}
-							count++;
-							if(count == 525){
-								console.log('<----- done ------>'+words[1]);
-								console.log(sounds);
-								message.channel.send(`\[${sounds.map( (s, index) => `\'${s}\'`).join(', ')}, \'other\'\]`);
-							}
-						}).catch( (e) => {
-							count++;
-							if(count == 525){
-								console.log('<----- done ------>'+words[1]);
-								console.log(sounds);
-								message.channel.send(`\[${sounds.map( (s, index) => `\'${s}\'`).join(', ')}, \'other\'\]`);
-							}
-						} );
-					}
-				}
-				break;
-				
-			case 'sound':
-				let c = [];
-				for(let i = 0; i < characters.ssr.length; i++){
-					request.head('http://game-a5.granbluefantasy.jp/assets/sound/voice/' + characters.ssr[i].id + '_' + words[1] + '.mp3').then( res => {
-						message.channel.send(`${characters.ssr[i].name}`);
-					}).catch(e => {});
-				}
-				message.channel.send(`<----- done ----->`);
-				break;
-				
-			case 'anime':
-				commands.anime.dothis(message);
-				break;
-				
-			default: message.react('⛔');
+		let cmd = words[0].substring(1);
+		let command = bot.commands.get(cmd) || null;
+		if(command){ 
+			command.run(bot, message, words);
+		}else{
+			message.react('⛔');
 		}
+			// case 'gay':
+			// case 'gey':
+			// case 'gai':
+			// case 'gei':
+			// case 'ugay':
+			// case 'ugai':
+			// case 'ugei':
+			// case 'ugey':
+				// message.channel.send('No, u.');
+				// break;
+				
+			// case 'play':
+				// if(!message.member.voiceChannel){
+					// message.react('🔇');
+					// return;
+				// }
+				// if (!servers[message.guild.id].songs[0]) return message.channel.send('Nothing to play.');
+				// if (servers[message.guild.id].playing) return message.channel.send("I'm already playing!!!");
+				
+				// servers[message.guild.id].playing = true;
+				
+				// (function play(song) {
+					// dispatcher = {};
+					// if (!song){
+						// servers[message.guild.id].playing = false;
+						// return;
+					// }
+					// currentlyPlayed = song.title;
+					
+					// dispatcher = message.guild.voiceConnection.playStream(yt(song.url, { filter: 'audioonly' }), { seek: 1, passes: 4 });
+					// dispatcher.on('end', () => {
+						// play(servers[message.guild.id].songs.shift());
+					// });
+					// dispatcher.on('error', () => {
+						// return message.channel.sendMessage('Shit happened.').then(() => {
+							// play(servers[message.guild.id].songs.shift());
+						// });
+					// });
+				// })(servers[message.guild.id].songs.shift());
+				// break;
+				
+			// case 'np': 
+			//if(servers[message.guild.id].playing) message.channel.send(`\`\`\`Playing: ${currentlyPlayed}\`\`\``);
+				// break;
+				
+			// case 'skip':
+				// if(!message.member.voiceChannel) return message.react('🔇');
+				// if(servers[message.guild.id].playing) dispatcher.end();
+				// break;
+				
+			// case 'test':
+				// sounds = [];
+				// let count = 1;
+				
+				// for(let i = 0; i < soundNames.length; i++){
+					// for(let j = 1; j < 4; j++){
+						// for(let k = 0; k < adds.length; k++){
+							// request.head('http://game-a5.granbluefantasy.jp/assets/sound/voice/'+ words[1] +'_'+soundNames[i]+j+adds[k]+'.mp3').then( res => {
+								// if(!sounds.includes(soundNames[i])){
+									// sounds[i] = soundNames[i];
+								// }else{}
+								// count++;
+							// }).catch(e => {
+								// count++;
+							// });
+						// }
+					// }
+				// }
+				
+				// for(let i = 0; i < soundNames.length; i++){
+					// for(let j = 1; j < 4; j++){
+						// request.head('http://game-a5.granbluefantasy.jp/assets/sound/voice/'+ words[1] +'_'+soundNames[i]+'0'+j+'.mp3').then( res => {
+							// if(!sounds.includes(soundNames[i])){
+									// sounds[i] = soundNames[i];
+							// }else{}
+							// count++;
+							// if(count == 525){
+								// console.log('<----- done ------>'+words[1]);
+								// console.log(sounds);
+								// message.channel.send(`\[${sounds.map( (s, index) => `\'${s}\'`).join(', ')}, \'other\'\]`);
+							// }
+						// }).catch( (e) => {
+							// count++;
+							// if(count == 525){
+								// console.log('<----- done ------>'+words[1]);
+								// console.log(sounds);
+								// message.channel.send(`\[${sounds.map( (s, index) => `\'${s}\'`).join(', ')}, \'other\'\]`);
+							// }
+						// } );
+					// }
+				// }
+				// break;
+				
+			// case 'sound':
+				// let c = [];
+				// for(let i = 0; i < characters.ssr.length; i++){
+					// request.head('http://game-a5.granbluefantasy.jp/assets/sound/voice/' + characters.ssr[i].id + '_' + words[1] + '.mp3').then( res => {
+						// message.channel.send(`${characters.ssr[i].name}`);
+					// }).catch(e => {});
+				// }
+				// message.channel.send(`<----- done ----->`);
+				// break;
 	}
 });
 
@@ -349,12 +219,25 @@ bot.on('messageReactionAdd', emo => {
 });
 
 
+bot.on('guildCreate', g => {
+	console.log(`     ${client.user.username} has joined to ${g.name}.`);
+	g.cmdPrefix = '.';
+	g.msgCounter = 0;
+	g.lastMsg = '';
+	g.music = {
+		playing: false,
+		songs: [],
+		voiceChannel: null,
+		currentlyPlayed: '',
+		dispatcher: {}
+	};
+});
+
 bot.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find(ch => ch.name === 'general');
   if (!channel) return;
-  channel.send(`<----- Welcome to the server, ${member}. \n Have fun with these guys. ----->`);
+  channel.send(`<----- Welcome to the server, ${member}. Have fun with these guys. ----->`);
 });
 
 
 bot.login(botToken);
-// bot.login(BOT_TOKEN);
