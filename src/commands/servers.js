852723186d0006cf568057a4d9299b88b8c56c0b
Paak0 <41326576+Paak0@users.noreply.github.com>
2019-04-19 16:06:07 +0200

    Add files via upload

--- src/commands/servers.js
+++ src/commands/servers.js
@@ -3,6 +3,6 @@ module.exports = {
 	help: 'List of servers abused by Belial.',
 	alias: [],
 	run: (bot, msg) => {
-		return msg.channel.send(`Belial is currently invading ${bot.guilds.size} servers:\n${bot.guilds.map( g => '- '+g.name )}`);
+		return msg.channel.send(`Belial is currently invading ${bot.guilds.size} servers:${bot.guilds.map( g => '\n'+'- '+g.name )}`);
 	}
 }
