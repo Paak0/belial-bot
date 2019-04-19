a3ae7dc3fc24f154c1095197fffcb25fd7ca1c3b
Paak0 <41326576+Paak0@users.noreply.github.com>
2019-04-19 23:27:12 +0200

    Add files via upload

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,15 +1,21 @@
 const yt = require('ytdl-core');
 
 let play = function(botto, message){
+	console.log('5');
 	let b = botto.guilds.get(message.guild.id).music;
+	console.log('6');
 	b.dispatcher = {};
 	b.currentlyPlayed = b.songs.shift();
+	console.log('7');
+	console.log(b.currentlyPlayed);
 	if (!b.currentlyPlayed){
 		b.playing = false;
 		b.currentlyPlayed = {};
 		return 0;
 	}
+	console.log('8');
 	let connect = b.voiceChannel;
+	console.log('9');
 	let disp = connect.playStream(yt(b.currentlyPlayed.url, { filter: 'audioonly' }), { seek: 0 });
 	disp.on('end', () => {
 		play(botto, message);
@@ -20,7 +26,9 @@ let play = function(botto, message){
 			play(botto, message);
 		});
 	});
+	console.log('10');
 	botto.guilds.get(message.guild.id).music.dispatcher = disp;
+	console.log('11');
 }
 
 module.exports = {
@@ -32,14 +40,14 @@ module.exports = {
 		let userCh = msg.member.voiceChannel.connection;
 		
 		if(!userCh || !botCh || userCh !== botCh) return msg.react('🔇');
-			
+		console.log('1');
 		let g = bot.guilds.get(msg.guild.id).music;
-		
+		console.log('2');
 		if(!g.songs[0]) return msg.channel.send('Nothing to play.');
 		if(g.playing) return msg.channel.send("I'm already playing!!!");
-		
+		console.log('3');
 		g.playing = true;
-		
+		console.log('4');
 		play(bot, msg);
 	}
 }
