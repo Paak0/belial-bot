8c5bec826ab5e646cfaddc01b3bdf25621c09457
Paak0 <41326576+Paak0@users.noreply.github.com>
2019-06-25 17:47:44 +0200

    Update belial.js

--- src/belial.js
+++ src/belial.js
@@ -2,20 +2,10 @@
 module.exports = {
 	
 	name: 'Belial',
-	desc: 'The Archangel of Cumming, Belial',
-	words: ["sodomy", "sex", "anal", "libido", "orgasm", "cum", "cumming", "climax", "oppai"],
+	desc: '',
+	words: [""],
 	
-	messages: [
-		"UHOHOHOOOHO!", "Mhmmm, do you want to be my koneko-chuan?", "Let me show you my powerfull libido.", 
-		"Saikou no ecstasy!!!", "Gahaha!!! The golden ratio! Yabai, i'll shit myself!!!", "I am all yours.", 
-		"Yaas, pleasure me more.", "I wouldn't mind letting you smash me.", "Let's do some sodomy.", "Perfection.",
-		"Oi, wanna some sodomy?", "Ooh, yeah! We're definitely doing THIS!", "You're getting me all hot and bothered.", 
-		"Our first time ought to be perfect.", "Faa-san is better."
-	],
+	messages: [""],
 	
-	otherMessages: [
-		"Try adding more sodomy.", "Yare yare daze.", "I don't think so.", "How about no?", 
-		"Have you seen my dog?", "That's not what i was expecting from you."
-	]
-	
+	otherMessages: [""]
 }
